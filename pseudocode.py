0f4981a840858211d3847b26666576d2388bf8fc
Graham Preston <graham.preston@gmail.com>
2017-01-05 11:18:50 -0500

    A lot of stuff

---
+++ pseudocode.py
@@ -0,0 +1,122 @@
+def main():
+    filename = "code.txt"
+    try:
+        with open(filename) as file:
+            code = file.read()
+        
+        # change code to Python
+        code = transcode(code)
+               
+        # execute the newly created Python code
+        exec(code)
+        
+    except FileNotFoundError:
+        print(filename + " not found.")
+        
+def transcode(code):
+    # fix indentation
+    code = code.replace('\t', '')
+    
+    layers = 0
+    for i in range(len(code)):
+        if code[i] == '{':
+            layers += 1
+        elif code[i] == '}':
+            layers -= 1
+        elif code[i] == '\n' and layers > 0:
+            if code[i+1] == '}':
+                code = code[:i+1] + ('\t' * (layers - 1)) + code[i+1:]
+            else:
+                code = code[:i+1] + ('\t' * layers) + code[i+1:]
+          
+    # fix odd characters
+    # NOTE: greater than and less than symbols must be fixed in the original file
+    # greater than: >=, less than: <=
+    for i in range(len(code)):
+        if ord(code[i]) == 8592:
+            code = code[:i] + '<-' + code[i+1:]
+        elif ord(code[i]) == 8800:
+            code = code[:i] + '!=' + code[i+1:]
+            
+    # change syntax
+    if "RANDOM" in code:
+        code = "import random\n" + code
+    
+    replacements = {
+        ' = '          : '==',
+        '<-'           : '=',
+        '\n{'          : ':',
+        ' {'           : ':',
+        '}'            : '',
+        "DISPLAY"      : "print",
+        " AND "        : " and ",
+        " OR "         : " or ",
+        "NOT"          : "not",
+        "IF"           : "if",
+        "ELSE"         : "else",
+        "PROCEDURE"    : "def",
+        "FOR EACH"     : "for",
+        "REPEAT UNTIL" : "while not",
+        " IN "         : " in ",
+        "RETURN"       : "return",
+        "INPUT"        : "input",
+        "LENGTH"       : "len",
+        "RANDOM"       : "random.randint"
+    }
+    
+    equals_replaced = False
+    for r in replacements:
+        if r == ' = ':
+            equals_replaced = True
+        if not (r == ' = ' and equals_replaced):
+            code = code.replace(r, replacements[r])
+    
+    for i in range(len(code)):
+        if code[i:i+6] == "REPEAT":
+            line = code[i:]
+            index = line.index('\n')
+            times = int(line[:index].split()[1])
+            
+            new_line = f"for i in range({times}):"
+            
+            code = code[:i] + new_line + code[index:]
+        
+        elif code[i:i+6] == "APPEND":
+            param_open, param_close = find_params(code, i, 6)
+
+            list_name, value = [p.strip() for p in code[i+param_open:i+param_close].split(',')]
+            append_py = f"{list_name}.append({value})"
+            
+            code = code[:i] + append_py + code[i+param_close+1:]
+        
+        elif code[i:i+6] == "REMOVE":
+            param_open, param_close = find_params(code, i, 6)
+
+            params = [p.strip() for p in code[i+param_open:i+param_close].split(',')]
+            remove_py = f"del {params[0]}[{params[1]}]"
+            
+            code = code[:i] + remove_py + code[i+param_close+1:]
+            
+        elif code[i:i+6] == "INSERT":
+            param_open, param_close = find_params(code, i, 6)
+
+            params = [p.strip() for p in code[i+param_open:i+param_close].split(',')]
+            insert_py = f"{params[0]}.insert({params[1]}, {params[2]})"
+            
+            code = code[:i] + insert_py + code[i+param_close+1:]
+        
+    return code
+        
+def find_params(s, i, j):
+    parameter_open = 0
+    while s[i+j-1] != ')':
+        if parameter_open == 0 and s[i+j] == "(":
+            parameter_open = j+1
+        elif s[i+j] == ")":
+            parameter_close = j
+        j += 1
+    
+    return [parameter_open, parameter_close]
+        
+if __name__ == '__main__':
+    main()
---
+++ robot.py
@@ -0,0 +1,109 @@
+import sys
+import pseudocode
+
+grid = []
+
+def main():
+    global grid
+    
+    with open("grid.txt") as file:
+        for line in file.readlines():
+            current_line = []
+            
+            for square in line.rstrip():
+                current_line.append(square)
+            grid.append(current_line)
+    
+    if not rectangular(grid):
+        print("The grid is not rectangular.")
+        sys.exit()
+        
+    execute(grid)
+    
+# may make different functions for transcoding and executing, or may change pseudocode.py to be like this
+def execute(grid, filename="robot_code.txt"):
+    with open(filename) as file:
+        code = file.read()
+        
+    code = pseudocode.transcode(code)
+    
+    facing = 4
+    directions = ('^', '>', 'v', '<')
+    
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[r][c] in directions and facing == 4:
+                if facing != 4:
+                    print("You cannot have more than one start position.")
+                    sys.exit()
+                facing = directions.index(grid[r][c])
+                location = [r, c]
+            
+    if facing == 4:
+        print("You must have a start position.")
+        sys.exit()
+        
+    grid[location[0]][location[1]] = '.'
+    
+    start_code = (
+        f"grid = {grid}\n"
+        "moves = ((-1, 0), (0, 1), (1, 0), (0, -1))\n"
+        f"facing = {facing}\n"
+        f"location = {location}\n"
+        "\n"
+        "def can_move(direction):\n"
+        "    coords = [x + y for x, y in zip(location, moves[direction])]\n"
+        "    if coords[0] < len(grid) and coords[1] < len(grid[1]) and grid[coords[0]][coords[1]] == '.':\n"
+        "        return True\n"
+        "    else:\n"
+        "        return False\n"
+        "\n"
+        "def move_forward():\n"
+        "    global moves\n"
+        "    global facing\n"
+        "    global location\n"
+        "    global can_move\n"
+        "    new = [x + y for x, y in zip(location, moves[facing])]\n"
+        "    if can_move(facing):\n"
+        "        location = new\n"
+        "        print(location)\n"
+        "\n"
+        "def rotate_right():\n"
+        "    global facing\n"
+        "    facing = (facing + 1) % 4\n"
+        "\n"
+        "def rotate_left():\n"
+        "    global facing\n"
+        "    facing = (facing - 1) % 4"
+        "\n"
+    )
+    
+    if code.startswith("import random"):
+        code = code[:14] + '\n' + start_code + code[14:]
+    else:
+        code = start_code + code
+        
+    replacements = {
+        "CAN_MOVE ("         : "CAN_MOVE(",
+        "CAN_MOVE(forward)"  : "can_move(facing)",
+        "CAN_MOVE(left)"     : "can_move((facing - 1) % 4)",
+        "CAN_MOVE(right)"    : "can_move((facing + 1) % 4)",
+        "MOVE_FORWARD"       : "move_forward",
+        "ROTATE_LEFT"        : "rotate_left",
+        "ROATE_RIGHT"        : "rotate_right"
+    }
+    
+    for _ in range(2):
+        for r in replacements:
+            code = code.replace(r, replacements[r])
+    
+    exec(code)
+
+def rectangular(lst):
+    for i in lst:
+        if len(i) != len(lst[0]):
+            return False
+    return True
+    
+if __name__ == '__main__':
+    main()
